afed1c01e4397f461d3ab0dc547bd3e1e62656a0
wang <374951963@qq.com>
2016-08-09 15:45:10 +0800

    完成顶部栏设置

--- app/src/main/java/com/example/wang/mynotedemo/MainActivity.java
+++ app/src/main/java/com/example/wang/mynotedemo/MainActivity.java
@@ -2,12 +2,10 @@ package com.example.wang.mynotedemo;
 
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -18,11 +16,12 @@ import com.github.clans.fab.FloatingActionMenu;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ScrollableRecyclerView contactsListView;
     List<Person> mPersons;
     FloatingActionMenu floatingActionMenu;
+    com.github.clans.fab.FloatingActionButton f1, f2, f3;
     NestedScrollView nestedScrollView;
     boolean isFloatingMenuHide = false;
 
@@ -54,8 +53,14 @@ public class MainActivity extends AppCompatActivity {
         nestedScrollView = (NestedScrollView) findViewById(R.id.nested_scroll_view);
 
         floatingActionMenu = (FloatingActionMenu) findViewById(R.id.menu_green);
-        MyRecyclerAdapter myRecyclerAdapter = new MyRecyclerAdapter(mPersons, this);
+        f1 = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.f1);
+        f2 = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.f2);
+        f3 = (com.github.clans.fab.FloatingActionButton) findViewById(R.id.f3);
+        f1.setOnClickListener(this);
+        f2.setOnClickListener(this);
+        f3.setOnClickListener(this);
 
+        MyRecyclerAdapter myRecyclerAdapter = new MyRecyclerAdapter(mPersons, this);
         contactsListView.setLayoutManager(new LinearLayoutManager(this));
         contactsListView.setAdapter(myRecyclerAdapter);
         nestedScrollView.setOnScrollChangeListener(
@@ -104,5 +109,23 @@ public class MainActivity extends AppCompatActivity {
     private void onScrollDown() {
         Log.d("onScrollDown", "执行了");
         showFloatingActionMenu();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.f1:
+                Snackbar.make(nestedScrollView, "点击了f1", Snackbar.LENGTH_SHORT).show();
+                floatingActionMenu.toggle(true);
+                break;
+            case R.id.f2:
+                Snackbar.make(nestedScrollView, "点击了f2", Snackbar.LENGTH_SHORT).show();
+                floatingActionMenu.toggle(true);
+                break;
+            case R.id.f3:
+                Snackbar.make(nestedScrollView, "点击了f3", Snackbar.LENGTH_SHORT).show();
+                floatingActionMenu.toggle(true);
+                break;
+        }
     }
 }
